00d0038cdde90d8d949a5ea67ef7c38877bc80ca
Matyas Hub <hubmatyas@gmail.com>
2018-10-17 13:18:44 +0200

    ewbetge

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector("#today").value = today;
 
-document.querySelector("#today2").valueAsDate = new Date();
+var output = {};
+jQuery(form).serializeArray().map(function(item) {
+    if ( output[item.name] ) {
+        if ( typeof(output[item.name]) === "string" ) {
+            output[item.name] = [output[item.name]];
+        }
+        output[item.name].push(item.value);
+    } else {
+        output[item.name] = item.value;
+    }
+});
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+download(output, 'output.json', 'application/json');
